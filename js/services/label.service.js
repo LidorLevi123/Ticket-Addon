5c266e470b385d0bf7691b00a36a9e1e19689fd0
Lidor-work <lidorlevi728@gmail.com>
2025-05-08 14:48:37 +0300

    add styling and ids to labels

--- js/services/label.service.js
+++ js/services/label.service.js
@@ -17,25 +17,24 @@ function _createLabels() {
     if(labels && labels.length) gLabels = labels
 
     gLabels = [
-        { color: '#808080', title: '' }, // gray
-        { color: '#8B0000', title: '' }, // dark red
-        { color: '#FF0000', title: '' }, // red
-        { color: '#FF8C00', title: '' }, // orange
-        { color: '#FFFF00', title: '' }, // yellow
-        { color: '#008000', title: '' }, // green
-        { color: '#00CED1', title: '' }, // turquoise/cyan
-        { color: '#0000CD', title: '' }, // blue
-        { color: '#800080', title: '' }, // purple
-      
-        { color: '#D3D3D3', title: '' }, // light gray
-        { color: '#A0522D', title: '' }, // brown
-        { color: '#FFB6C1', title: '' }, // pink
-        { color: '#FFE4B5', title: '' }, // peach/light orange
-        { color: '#F5F5DC', title: '' }, // beige
-        { color: '#ADFF2F', title: '' }, // light green
-        { color: '#AFEEEE', title: '' }, // light blue
-        { color: '#87CEFA', title: '' }, // soft blue
-        { color: '#D8BFD8', title: '' }  // light purple
+        { id: 'l-100', color: '#808080', title: '' }, // gray
+        { id: 'l-101', color: '#8B0000', title: '' }, // dark red
+        { id: 'l-102', color: '#FF0000', title: '' }, // red
+        { id: 'l-103', color: '#FF8C00', title: '' }, // orange
+        { id: 'l-104', color: '#FFFF00', title: '' }, // yellow
+        { id: 'l-105', color: '#008000', title: '' }, // green
+        { id: 'l-106', color: '#00CED1', title: '' }, // turquoise/cyan
+        { id: 'l-107', color: '#0000CD', title: '' }, // blue
+        { id: 'l-108', color: '#800080', title: '' }, // purple
+        { id: 'l-109', color: '#D3D3D3', title: '' }, // light gray
+        { id: 'l-110', color: '#A0522D', title: '' }, // brown
+        { id: 'l-111', color: '#FFB6C1', title: '' }, // pink
+        { id: 'l-112', color: '#FFE4B5', title: '' }, // peach/light orange
+        { id: 'l-113', color: '#F5F5DC', title: '' }, // beige
+        { id: 'l-114', color: '#ADFF2F', title: '' }, // light green
+        { id: 'l-115', color: '#AFEEEE', title: '' }, // light blue
+        { id: 'l-116', color: '#87CEFA', title: '' }, // soft blue
+        { id: 'l-117', color: '#D8BFD8', title: '' }  // light purple
     ]
     _saveLabels()
 }
