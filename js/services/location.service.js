18fa5c1d38a540121157f29f2806ed5e5a9d7c01
LidorLevi123 <lidorlevi728@gmail.com>
2025-05-22 22:38:21 +0300

    change copy btn icon

--- js/services/location.service.js
+++ js/services/location.service.js
@@ -242,11 +242,16 @@ function addShareButtons(anchor, recid, personName, mobileNumber, location, desc
   const whatsappLink = `https://wa.me/?text=${encodeURIComponent(textToShare)}`;
 
   // Copy to clipboard button
-  const copyBtn = document.createElement("button");
-  copyBtn.innerText = "📋";
-  copyBtn.type = 'button'
+  const copyBtn = document.createElement("span");
+  copyBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="-1 0 20 20">
+  <g id="copy-4" transform="translate(-3 -2)">
+    <path id="secondary" fill="#2ca9bc" d="M19,3H16V4a1,1,0,0,1-1,1H13a1,1,0,0,1-1-1V3H9A1,1,0,0,0,8,4V16a1,1,0,0,0,1,1H19a1,1,0,0,0,1-1V4A1,1,0,0,0,19,3Z"/>
+    <path id="primary" d="M16,17v3a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V8A1,1,0,0,1,5,7H8" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+    <path id="primary-2" data-name="primary" d="M19,3H9A1,1,0,0,0,8,4V16a1,1,0,0,0,1,1H19a1,1,0,0,0,1-1V4A1,1,0,0,0,19,3ZM12,4a1,1,0,0,0,1,1h2a1,1,0,0,0,1-1V3H12Z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+  </g>
+</svg>`
   copyBtn.title = "העתק ללוח";
-  copyBtn.style.cssText = "margin-left:5px; cursor:pointer; font-size:12px;";
+  copyBtn.style.cursor = "pointer";
   copyBtn.onclick = () => {
     navigator.clipboard.writeText(textToShare)
     .then(() => {
@@ -256,6 +261,20 @@ function addShareButtons(anchor, recid, personName, mobileNumber, location, desc
       showErrorMsg('לא ניתן להעתיק ללוח')
     });
   };
+  // const copyBtn = document.createElement("button");
+  // copyBtn.innerText = "📋";
+  // copyBtn.type = 'button'
+  // copyBtn.title = "העתק ללוח";
+  // copyBtn.style.cssText = "margin-left:5px; cursor:pointer; font-size:12px;";
+  // copyBtn.onclick = () => {
+  //   navigator.clipboard.writeText(textToShare)
+  //   .then(() => {
+  //     showUserMsg(`פניה ${recid} הועתקה ללוח`)
+  //   })
+  //   .catch(() => {
+  //     showErrorMsg('לא ניתן להעתיק ללוח')
+  //   });
+  // };
 
   // WhatsApp share button with image
   const waBtn = document.createElement("a");
@@ -272,7 +291,15 @@ function addShareButtons(anchor, recid, personName, mobileNumber, location, desc
 
   waBtn.appendChild(waIcon);
 
+  const container = document.createElement('div')
+  container.style.display = 'flex'
+  container.style.gap = '5px'
+  container.style.justifyContent = 'center'
+  container.style.marginBlockStart = '5px'
+
   // Append buttons to the <td>
-  td.appendChild(copyBtn);
-  td.appendChild(waBtn);
+  container.appendChild(copyBtn);
+  container.appendChild(waBtn);
+
+  td.appendChild(container)
 }
